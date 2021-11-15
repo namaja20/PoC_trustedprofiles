1e72b555e75a9bea204a1c6d84d0dade76162ee9
namaja20 <60924585+namaja20@users.noreply.github.com>
2021-11-15 16:35:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-}
+//provider "ibm" {
+//  ibmcloud_api_key = var.ibmcloud_api_key
+//}
 
 // Provision iam_trusted_profile resource instance
 resource "ibm_iam_trusted_profile" "iam_trusted_profile_instance" {
